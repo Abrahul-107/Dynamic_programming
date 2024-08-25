df0e80e6f3dd932de7e14f37b40bf97ef4f8fbdd
Abrahul-107 <Abrahul-107>
2024-08-25 23:27:17 +0530

    LIS memoization

--- Dp_problems_Misc/07_longestIncreasingSubsequence.cpp
+++ Dp_problems_Misc/07_longestIncreasingSubsequence.cpp
@@ -2,6 +2,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int LISRecursion(int ind,int prev,vector<int>&arr,int n,vector<vector<int>>&dp)
+{
+    if(ind==n)
+        return 0;
+    if(dp[ind][prev+1]!=-1)
+        return dp[ind][prev+1];
+    int len = 0 + LISRecursion(ind+1,prev,arr,n,dp);
+    if(prev==-1 || arr[ind]>arr[prev])
+        len = max(len,1+LISRecursion(ind+1,ind,arr,n,dp));
+    return dp[ind][prev+1] = len;
+
+}
+
+
 int longestIncreasingSubsequence(vector<int>&arr)
 {
     int n = arr.size();
@@ -17,7 +31,11 @@ int longestIncreasingSubsequence(vector<int>&arr)
 int main()
 {
     vector<int>v = {10,2,4,3,1,45,0};
+    int n = v.size();
     cout<<longestIncreasingSubsequence(v)<<endl;
+    vector<vector<int>>dp(n,vector<int>(n+1,-1));
+
+    cout<<LISRecursion(0,-1,v,n,dp);
 
     return 0;
 
